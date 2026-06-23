5bbe0660df53c98747538aa4c72032b6da8dbc45
Steve Townsend <SteveTownsend0@gmail.com>
2026-06-23 14:28:59 -0400

    fix runtime type-check

--- FacegenBaseline/Program.cs
+++ FacegenBaseline/Program.cs
@@ -55,8 +55,8 @@ namespace FacegenBaseline
                 // Compare winning override Head Parts with master - if this record is already overriding the appearance, we let it win
                 var master = contexts.Last();
                 // If only one of winner or master uses Traits, that is also a mismatch
-                if (master.Record.Configuration.Flags.HasFlag(NpcConfiguration.TemplateFlag.Traits) !=
-                    currentWinner.Record.Configuration.Flags.HasFlag(NpcConfiguration.TemplateFlag.Traits))
+                if (master.Record.Configuration.TemplateFlags.HasFlag(NpcConfiguration.TemplateFlag.Traits) !=
+                    currentWinner.Record.Configuration.TemplateFlags.HasFlag(NpcConfiguration.TemplateFlag.Traits))
                 {
                     Console.WriteLine("Appearance for {0}/{1:X8} from Traits in {2}", baselineNPC.Name, baselineNPC.FormKey.ID, currentWinner.ModKey.FileName);
                     ++hasBetterFacegen;
